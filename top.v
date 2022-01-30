822bdd036d417a7301fceabe248644a5dc50bd2f
Ben Clifford <benc@hawaga.org.uk>
2022-01-30 22:07:16 +0000

    Add instructions for load, subtraction and conditional jump
    This lets the demo program flash the LED 3 times in a row,
    with a longer break in between

--- top.v
+++ top.v
@@ -22,7 +22,9 @@ module top (
 
     reg [31:0] instr;
 
-    reg [31:0] delay_countdown; // for 0020 DELAY instruction
+    reg [31:0] delay_countdown; // for 1xxxxxxx DELAY instruction
+
+    reg [31:0] scratch; // simple register file of 1 register... elaborate later
 
     always @(posedge CLK) begin
       if(rst_delay == 0) begin
@@ -57,6 +59,25 @@ module top (
             pc <= 0;
             instr_phase <= 0;
           end
+          if( (instr & 32'hF0000000) == 32'h40000000) begin // subtract scratch immediate
+            scratch <= scratch - (instr & 32'h0FFFFFFF);
+            pc <= pc + 1;
+            instr_phase <= 0;
+          end
+           if( (instr & 32'hF0000000) == 32'h50000000) begin // jump back if non-zero, relative immediate 5
+            if (scratch != 0) begin
+              pc <= pc - (instr & 32'h0FFFFFFF);
+            end else begin
+              pc <= pc + 1;
+            end
+            instr_phase <= 0;
+          end
+          if( (instr & 32'hF0000000) == 32'h60000000) begin // load scratch immediate
+            scratch <= instr & 32'h0FFFFFFF;
+            pc <= pc + 1;
+            instr_phase <= 0;
+          end
+ 
         end
         if(instr_phase == 2) begin // someones requested a delay before going back to phase 0
             if(delay_countdown == 0) begin
@@ -79,6 +100,25 @@ module top (
       pc = 0;
       instr_phase = 0;
 
+
+      ram[0] = 32'h60000003; // load scratch immediate <- 3
+
+      // this block three times
+      ram[1] = 32'h20000001; // LED on
+      ram[2] = 32'h10200000; // sleep a blip
+      ram[3] = 32'h20000000; // LED off
+      ram[4] = 32'h10800000; // sleep 0.5s
+
+      ram[5] = 32'h40000001; // some kind of decrement top of stack by one?
+
+      ram[6] = 32'h50000005; // some kind of jump-back if non-zero / jump if zero - jump back relative, 5
+
+      // NO-OP as no stack register file now  ram[6] = 32'h70000000; // some kind of drop from stack, if using stack style registers
+ 
+      ram[7] = 32'h15000000; // sleep 5s
+      ram[8] = 32'h30000000; // reset PC to start - jump-unconditional to start
+
+/*
       ram[0] = 32'h20000001; // LED on
       ram[1] = 32'h11000000; // sleep 1s
       ram[2] = 32'h20000000; // LED off
@@ -88,6 +128,7 @@ module top (
       ram[6] = 32'h20000000; // LED off
       ram[7] = 32'h11000000; // sleep 1s
       ram[8] = 32'h30000000; // reset PC to start
+*/
     end;
 
 endmodule
