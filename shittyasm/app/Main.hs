465cfd38f9b2c2cb4287f0447f510497ee8f5c7e
Ben Clifford <benc@hawaga.org.uk>
2022-02-05 12:53:04 +0000

    Change ringing style beeps to a one-shot startup-time 'bbc pips' sequence

--- shittyasm/app/Main.hs
+++ shittyasm/app/Main.hs
@@ -36,12 +36,13 @@ emitToken t = error $ "non-emittable token " ++ show t
 
 myprog = [
     CONSOLEUARTINIT,
+    GOSUB 30,
     TONEGEN 0,
     LOAD 3,
     CONSOLEWRITE 83, -- 83 is the code for Y - could write haskell compile time code for this...
     LOAD 5,
     LOAD 20,
-    GOSUB 23,
+    GOSUB 25,
     DECR 1,
     JUMPBACKNZ 2,
     -- GOSUB 18,  -- if I move this gosub up one line to immediately before the drop, I get the inner loop repeated blipblip-long, without ever getting the final delay. But I do get the delay that happens right after this gosub, by the looks of things? (visually)
@@ -60,13 +61,23 @@ myprog = [
     DROP,
     CONSOLEWRITE 82, -- 82 is the code for R - could write haskell compile time code for this...
     SLEEP 0x5000000,
-    JUMPZERO,
-    TONEGEN 0xB000,
+    LOAD 3,
+    RET, -- RET >> LOAD = GOTO
     LED True,
     SLEEP 0x80000,
-    TONEGEN 0x4000,
     LED False,
     SLEEP 0x80000,
+    RET,
+    LOAD 3,
+    TONEGEN 0x4000,
+    SLEEP 1600000,
+    TONEGEN 00,
+    SLEEP 14400000,
+    DECR 1,
+    JUMPBACKNZ 5,
+    DROP,
+    TONEGEN 0x4000,
+    SLEEP 8000000,
     TONEGEN 0,
     RET
   ]
